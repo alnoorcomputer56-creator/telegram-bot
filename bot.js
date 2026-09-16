f71830b91c872f6766c4e0fe2223b337e0802e7a
alnoorcomputer56-creator <alnoorcomputer56@gmail.com>
2026-09-16 16:47:14 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,7 +34,8 @@ function escapeHtml(str) {
     return String(str)
         .replace(/&/g, "&")
         .replace(//g, ">")
-        .replace(/"/g, "");
+        .replace(/"/g, """)
+        .replace(/'/g, "'");
 }
 
 // Complete list of all countries with dial code and 2-digit ISO code
